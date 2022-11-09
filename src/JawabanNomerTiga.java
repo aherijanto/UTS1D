71d77ce136a6a11b2db0cdfa8e61b7464b60b2b6
bistique <aherijanto@gmail.com>
2022-11-09 01:56:40 -0600

    sudah selesai

--- src/JawabanNomerTiga.java
+++ src/JawabanNomerTiga.java
@@ -1,5 +1,7 @@
 public class JawabanNomerTiga {
     public static void main(String[] args) {
-        System.out.println("Hello Class 1D");
+       for(int i=0;i<10;i++){
+           System.out.println(i);
+       }
     }
 }
